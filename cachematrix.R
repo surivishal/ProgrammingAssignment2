581a7f06d0ccc62d397ea221b382cdabff8fdaad
surivishal <surivishal@gmail.com>
2015-03-22 14:48:48 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,42 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## this function makeCacheMatrix creates a matrix and calculates its inverse and assignes to a variable
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+set <- function(y){
+x <<- y
+inv <<- NULL
 }
+
+get <- function() x
+
+setinverse <- function(ext_inv){
+inv <<- ext_inv
+}
+getinverse <- function() {
+inv <<- solve(x)
+inv
+ }
+ 
+list(set = set, get =get, setinverse=setinverse, getinverse=getinverse)
+ }
+
 
 
 ## Write a short comment describing this function
-
+##this function takes the matrix created using makeCacheMatrix function and first checks if inverse of the matrix is calculated and is in cache
+## if inverse is not in cache, it creates the inverse of the matrix locally and assigns it to the variable local_matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)){
+                print("getting cached inverse")
+                return inv
+        }
+        
+        local_matrix <- x$get()
+        inv <- solve(local_matrix)
+        print("calculating inverse fresh locally")
+        return inv
 }
